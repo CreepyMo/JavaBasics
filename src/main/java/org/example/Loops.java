355047c2e3503ce98c3ee0af8763fc036337f647
Stas Blashchuk <stanislav.blashchuk@cinarra.com>
2025-03-06 19:30:53 +0700

    Add For Loop tasks

---
+++ src/main/java/org/example/Loops.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Loops {
+
+    public static void main(String[] args) {
+//        sumOfNumbers();
+//        reverseOrder();
+//        multiplicationTable(5);
+    }
+
+    public static void sumOfNumbers() {
+        /**
+         * Task: Write a program that calculates and prints the sum of numbers from 1 to 100 using a for loop.
+         * Expected output:
+         * The sum is: 5050
+         */
+    }
+
+    public static void reverseOrder() {
+        /**
+         * Task: Write a program that prints numbers from 10 to 1 in reverse order using a for loop.
+         * Expected output:
+         * 10 9 8 7 6 5 4 3 2 1
+         */
+    }
+
+    public static void multiplicationTable(int n) {
+        /**
+         * Task: Write a program that prints the multiplication table for a given number n (e.g., n = 5) using a for loop.
+         * Expected output:
+         * 5 x 1 = 5
+         * 5 x 2 = 10
+         * ...
+         * 5 x 10 = 50
+         */
+    }
+}
