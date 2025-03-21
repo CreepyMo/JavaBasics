cb5d68269d8f5fea3195c38504642538c817e897
Stas Blashchuk <stanislav.blashchuk@cinarra.com>
2025-03-21 22:02:26 +0200

    Added homework on loops deep dive

---
+++ src/main/java/org/example/LoopsDeepDive.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoopsDeepDive {
+    public static void main(String[] args) {
+//        reverseStarPyramid(10);
+    }
+
+    public static void longestWord() {
+        /**
+         Task: Find and print out the longest word from a given ArrayList.
+         Create two solutions. First one using the For loop, second one using the For-each loop
+         */
+
+        List<String> words = new ArrayList<>(List.of("banana", "constructors", "skyscraper", "cherry", "boss"));
+    }
+
+    public static void sumEvenNumbers() {
+        /**
+         Task: Print out the sum of EVEN numbers from a given ArrayList
+         Create two solutions. First one using the For loop, second one using the For-each loop
+         */
+
+        List<Integer> numbers = new ArrayList<>(List.of(1, 3, 22, 4, 6, 5, 5, 891, 900, -4));
+    }
+
+    public static void reverseStarPyramid(int n) {
+        /**
+         Write a Java program that prints a reversed star pyramid.
+
+         Example for n = 5:
+
+         *****
+         ****
+         ***
+         **
+         *
+
+         Hints:
+         - Use nested for loops
+         - Use System.out.print("*") in a nested loop in order to print stars on the same line
+         - Once you are out of a nested loop, use System.out.println() to create a line break
+
+         */
+    }
+}
