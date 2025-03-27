be553bb53c0dd056523a828d683a3558a92097c6
nkalinina73 <35932117+nkalinina73@users.noreply.github.com>
2025-03-27 13:54:58 -0700

    CombineStrings done

--- src/main/java/org/example/strings/CombineStrings.java
+++ src/main/java/org/example/strings/CombineStrings.java
@@ -3,14 +3,19 @@ package org.example.strings;
 public class CombineStrings {
 
     public static void main(String[] args) {
-        System.out.println(combineStrings("Hello ", "World!")); // Hello, World! should be printed to console
+        combineStr();
+      /*  System.out.println(combineStrings("Hello ", "World!")); // Hello, World! should be printed to console*/
     }
-
     /**
      Now let's learn how to concatenate strings using StringBuilder.
      In the combineStrings() method, join the input strings one and two.
      Return the new string as a result. Use the StringBuilder class to concatenate the strings.
      */
+    public static void combineStr(){
+        StringBuilder one = new StringBuilder("Hello ");
+        StringBuilder two = new StringBuilder("Word!");
+        System.out.println(one.append(two));
+    }
     public static String combineStrings(String one, String two) {
         // Create StringBuilder object for string 'one'
         // Append string 'two'
