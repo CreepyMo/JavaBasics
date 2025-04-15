e7e86420130d8a0d71fb2900bbf0a518374762c7
nkalinina73 <35932117+nkalinina73@users.noreply.github.com>
2025-04-15 16:13:31 -0700

    DoublePower is done

--- src/main/java/org/example/arrays/DoublePower.java
+++ src/main/java/org/example/arrays/DoublePower.java
@@ -2,33 +2,18 @@ package org.example.arrays;
 
 public class DoublePower {
 
-    /**
-     * In this task, you will learn how to iterate through an array.
-     * Implement the method getDoublePower() that takes an array of integers powers
-     * and returns a new array where each element's value is multiplied by 2.
-
-     * Examples:
-     * getDoublePower({});           // {}
-     * getDoublePower({1, -2});      // {2, -4}
-     * getDoublePower({45, 34, 56, 67}); // {90, 68, 112, 134}
-
-     * Hint 1:
-         - To double the value of each element, use a loop with the condition: for (int i = 0; i < powers.length; i++).
-
-     * Hint 2:
-         - Inside the loop, get the value of the element at index i and multiply it by 2.
-         - Assign the result back to the element at index i.
-
-     * Hint 3:
-         - At the end, return the result using the return keyword.
-     */
 
 
     public static void main(String[] args) {
-
+        getDoublePower(new int[]{45, 34, 56, 67});
     }
 
     public static int[] getDoublePower(int[] powers) {
+        int doubled = 0;
+        for (int i = 0; i < powers.length; i++){
+            doubled = powers[i] * 2;
+            System.out.println(doubled);
+        }
         return powers;
     }
 }
